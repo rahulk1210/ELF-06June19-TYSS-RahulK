d3517b29272f52c348c280da99f6ebdac0bc2cd0
rahulk <rahul.k@testyantra.com>
2019-06-18 17:48:00 +0530

    calculator related file

---
+++ calculator/src/com/testyantra/calculator/basic/BasicFunction.java
@@ -0,0 +1,18 @@
+package com.testyantra.calculator.basic;
+
+public class BasicFunction {
+	public  int add(int i,int j) {
+		return i+j;
+	}
+
+	public  int multi(int i,int j) {
+		return i*j;
+	}
+	public  int sub(int i,int j) {
+		return i-j;
+	}
+	public  int div(int i,int j) {
+		return i/j;
+	}
+}//end of the method
+
